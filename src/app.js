94e9cd175f6d6f6a9a339b479928d0ac31474fff
kamal126 <kamalchandra265@gmail.com>
2025-08-08 11:44:43 +0530

    feat: add profile route to serve user profile page

--- src/app.js
+++ src/app.js
@@ -6,5 +6,8 @@ app.use(express.json());
 app.get('/', (req, res)=>{
     res.send("<h1>Welcome to CareerBridge API</h1><p>Connecting students with verified university seniors for real-time guidance.</p>");
 });
+app.get('/profile', (req, res)=>{
+    res.send("<h1>Profile Page</h1><p>View and manage your profile here.</p><p><a href='/'>Go back to Home</a></p>");
+});
 
 export default app;

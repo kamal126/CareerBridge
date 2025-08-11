3b909415e46e0965f4387f093a6633c5d10c98be
kamal126 <kamalchandra265@gmail.com>
2025-08-11 08:17:56 +0530

    fix: remove commented-out logging statements in database connection logic

--- src/db/index.js
+++ src/db/index.js
@@ -5,11 +5,6 @@ const connectDB = async () => {
        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
         console.log(`DB NAME: ${connectionInstance.connection.name}`);
-        // console.log(`DB PORT: ${connectionInstance.connection.port}`);
-        // console.log(`DB USER: ${connectionInstance.connection.user}`);
-        // console.log(`DB STATE: ${connectionInstance.connection.readyState}`);
-        // console.log(`DB VERSION: ${connectionInstance.connection.serverConfig.version}`);
-        // console.log(`DB CLIENT: ${connectionInstance.connection.client.s.url}`);
     } catch (error) {
         console.log(`\n MONGODB connection FAILED: ${error.message}`);
         process.exit(1);

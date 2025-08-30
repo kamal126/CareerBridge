946355c73e4683fa659da358ebcf65ad0e141929
kamal126 <kamalchandra265@gmail.com>
2025-08-31 01:59:31 +0530

    change: (req, res, cb) -> (req, file, cb) and add: Date.now() in filename

--- Backend/src/middlewares/multer.middleware.js
+++ Backend/src/middlewares/multer.middleware.js
@@ -3,12 +3,12 @@ import multer from 'multer';
 // Multer configuration
 const storage = multer.diskStorage({
     // specify the destination directory for uploaded files
-    destination: function(req, res, cb){    // cb -> callback function
+    destination: function(req, file, cb){    // cb -> callback function
         cb(null, "public/temp") 
     },
     // specify the filename for the uploaded files
     filename: function(req, file, cb){
-        cb(null, Date.now() + "-" + file.originalname)
+        cb(null,Date.now() +"-" + file.originalname)
     }
 });
 

a8a37fcac21011e0ff6162a67a99aa7eb791927d
kamal126 <kamalchandra265@gmail.com>
2025-09-01 23:57:30 +0530

    auth middle ware check for user login or not and other refresh tokens

---
+++ Backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,36 @@
+import { ApiError  } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken"
+import { User } from "../models/user.model.js";
+
+export const verifyJWT = asyncHandler( async (req, _, next) => {
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorizaation")?.replace("Bearer ", "");
+    
+        console.log("token: ", token);
+        
+    
+        // 1. check if token is provided
+        if(!token){
+            throw new ApiError(401, "Unauthorized request, token missing");
+        }
+    
+         
+        const decodeToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    
+        const user = await User.findById(decodeToken?._id).select("-password -refreshToken");
+    
+        if(!user){
+            throw new ApiError(401, "Invalid access token or user not found");
+        }
+    
+        req.user = user;
+    
+        next()
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid access token");
+        
+    }
+})
+
+

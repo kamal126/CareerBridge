b82369ef82df58e867dc6e0e8f92f2f11ba4650c
kamal126 <kamalchandra265@gmail.com>
2025-09-03 21:24:31 +0530

    feature: change-password route

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -2,7 +2,8 @@ import { Router } from "express";
 import { registerUser,
             loginUser,
             logoutUser,
-            refershAccessToken
+            refershAccessToken,
+            updateCurrPassword
 
  } from "../contollers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
@@ -22,6 +23,7 @@ router.route("/login").post(loginUser)
 // secure routes
 router.route("/logout").post(verifyJWT, logoutUser) // verifyJWT middleware to protect the route
 router.route("/refresh-token").post(refershAccessToken) // no need to verifyJWT here as we are using refresh token
+router.route("/password-change").post(verifyJWT, updateCurrPassword)
 
 
 export default router;

5b8f635b305d192914501b1900e12c00b2805690
kamal126 <kamalchandra265@gmail.com>
2025-09-07 11:50:49 +0530

    deleted

--- Frontend/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">
-        🚀 CareerBridge – Connect Students with Seniors
-      </h1>
-    </div>
-  );
-}

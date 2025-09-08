3b8046408ce7b7cebcdd90a57cffaa3e545d5a02
kamal126 <kamalchandra265@gmail.com>
2025-09-08 17:46:47 +0530

    add feat: University feature

---
+++ Frontend/src/components/university/University.component.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function University() {
+  return (
+    <>
+      <section className="px-6 md:px-20 py-12 p-6">
+      <div className="max-w-4xl">
+        <h4 className="text-pink-500 font-semibold text-sm mb-2">
+          University
+        </h4>
+
+        <h2 className="text-3xl md:text-4xl font-bold text-gray-900 mb-3">
+          Visit University
+        </h2>
+
+        <div className="w-14 h-1 bg-blue-600 mb-6"></div>
+
+        <p className="text-gray-600 leading-relaxed text-base md:text-lg">
+          We are dedicated to providing world-class education, fostering
+          innovation, and shaping leaders of tomorrow. With a diverse range of
+          programs, state-of-the-art facilities, and a vibrant campus life, we
+          create an environment that inspires growth, creativity, and success.
+        </p>
+      </div>
+    </section>
+    
+    </>
+  );
+}
+
+export default University;
+

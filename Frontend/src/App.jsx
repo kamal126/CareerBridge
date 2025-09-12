5a269cc70168e5525bf47fcd55bcbe9e520c4430
kamal126 <kamalchandra265@gmail.com>
2025-09-12 13:52:01 +0530

    university component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,14 @@
-
 import './App.css'
 import Carousel from './components/Slider/Carousel.component.jsx'
+import About from './components/About/About.component.jsx'
+import University from './components/university/University.component.jsx'
 function App() {
 
   return (
     <>
     <Carousel/>
+    <About/>
+    <University/>
     </>
   )
 }
